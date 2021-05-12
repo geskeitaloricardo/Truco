ed5f8e5754af8ec493b951607d8ed963702b92a9
italo ricardo geske <italoricardogeske@gmail.com>
2021-05-12 23:40:51 +0900

    Initial Commit

---
+++ Polygons/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Polygons
+//
+//  Created by Italo Ricardo Geske on 2021/05/12.
+//  Copyright © 2021 Italo Ricardo Geske. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
